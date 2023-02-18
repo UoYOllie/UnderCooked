0e754fc1551ec689e6b3dad747146b4ada362384
ajbrown <ab3073@york.ac.uk>
2023-02-18 17:25:51 +0000

    Adding test for asset

---
+++ ENG1/tests/src/AssetTests.java
@@ -0,0 +1,12 @@
+import com.badlogic.gdx.Gdx;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+@RunWith(GdxTestRunner.class)
+public class AssetTests {
+    @Test
+    public void testChefAssetExists(){
+        assertTrue("This test will only pass if hold down.png", Gdx.files.internal("hold down.png").exists());
+    }
+}
