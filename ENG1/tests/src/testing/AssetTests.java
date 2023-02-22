d1745fbae91634a9edde0d447e770389cdf1b412
ajbrown <ab3073@york.ac.uk>
2023-02-22 11:25:13 +0000

    Tried to get movement test working

--- ENG1/tests/src/testing/AssetTests.java
+++ ENG1/tests/src/testing/AssetTests.java
@@ -55,8 +55,8 @@ public class AssetTests {
     }
 
     @Test
-    public void testCookAtlasAssetExists(){
-        assertTrue(Gdx.files.internal("cooks/cook atlas.tpproj").exists(), "This test will only pass if cook atlas.tpproj is present");
+    public void testPlayerAtlasAssetExists(){
+        assertTrue(Gdx.files.internal("cooks/Player atlas.tpproj").exists(), "This test will only pass if cook atlas.tpproj is present");
     }
 
     //The following are tests to do with checking that assets in the customers folder remain
@@ -76,8 +76,8 @@ public class AssetTests {
 
     @Test
     public void testAtlasCookAssetExists(){
-        assertTrue(Gdx.files.internal("atlas/cook.png").exists(),"This test will only pass if cook.png exists in the atlas folder");
-        assertTrue(Gdx.files.internal("atlas/cook.atlas").exists(),"This will only pass if customer.atlas exists");
+        assertTrue(Gdx.files.internal("atlas/player.png").exists(),"This test will only pass if cook.png exists in the atlas folder");
+        assertTrue(Gdx.files.internal("atlas/player.atlas").exists(),"This will only pass if customer.atlas exists");
     }
 
     @Test
---
+++ ENG1/tests/src/testing/MovementTest.java
@@ -0,0 +1,28 @@
+package testing;
+
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
+import game.GameScreen;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import static org.testng.Assert.*;
+
+@RunWith(GdxTestRunner.class)
+public class MovementTest {
+
+    @Test
+    public void TestChefMoveLeft(){
+        int x = 0;
+        int y = 0;
+        GameScreen gameScreen = new GameScreen();
+        Body body = new Body();
+
+        World world = new World(new Vector2(x,y),false);
+        cooks.Cook cook =- new cooks.Cook(1,1,body,gamescreen);
+        cook.userInput();
+    }
+}
