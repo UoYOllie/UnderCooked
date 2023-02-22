3e612b42d323706a83a6a2d7b5f0a968a589d902
Rebecca <rebsto2@gmail.com>
2023-02-22 10:10:50 +0000

    added asset tests for Individual_Stations and Maps folders

--- ENG1/tests/src/testing/AssetTests.java
+++ ENG1/tests/src/testing/AssetTests.java
@@ -54,6 +54,11 @@ public class AssetTests {
         assertTrue(Gdx.files.internal("cooks/normal up.png").exists(), "This test will only pass if normal up.png is present");
     }
 
+    @Test
+    public void testCookAtlasAssetExists(){
+        assertTrue(Gdx.files.internal("cooks/cook atlas.tpproj").exists(), "This test will only pass if cook atlas.tpproj is present");
+    }
+
     //The following are tests to do with checking that assets in the customers folder remain
     @Test
     public void testCustomerAssetExists(){
@@ -84,5 +89,78 @@ public class AssetTests {
     public void testAtlasStationAssetExists(){
         assertTrue(Gdx.files.internal("atlas/station.png").exists(),"This test will only pass if station.png exists in the atlas folder");
         assertTrue(Gdx.files.internal("atlas/station.atlas").exists(),"This will only pass if station.atlas exists");
+    }
+
+    //The following test that all assets in the Individual_Stations folder exist
+
+    @Test
+    public void testBinAssetExists(){
+        assertTrue(Gdx.files.internal("Individual_Stations/bin.png").exists(),"This will only pass if bin.png exists");
+    }
+
+    @Test
+    public void testCuttingStationAssetExists(){
+        assertTrue(Gdx.files.internal("Individual_Stations/Cutting_Station.png").exists(),"This will only pass if CuttingStation.png exists");
+    }
+
+    @Test
+    public void FryerAssetExists(){
+        assertTrue(Gdx.files.internal("Individual_Stations/Fryer.png").exists(),"This will only pass if Fryer.png exists");
+    }
+
+    @Test
+    public void testStationsTpprojAssetExists(){
+        assertTrue(Gdx.files.internal("Individual_Stations/stations.tpproj").exists(),"This will only pass if stations.tpproj exists");
+    }
+
+    @Test
+    public void testTableAssetExists(){
+        assertTrue(Gdx.files.internal("Individual_Stations/Table.png").exists(),"This will only pass if Table.png exists");
+    }
+    @Test
+    public void testTableWithChairsAssetExists(){
+        assertTrue(Gdx.files.internal("Individual_Stations/TableWithChairs.png").exists(),"This will only pass if TableWithChairs.png exists");
+    }
+
+    //The following test that all assets in the Maps folder exist
+
+    @Test
+    public void testStartMenuBackgroundAssetExists(){
+        assertTrue(Gdx.files.internal("Maps/StartMenuBackground.png").exists(),"This will only pass if StartMenuBackground.png exists");
+    }
+
+    @Test
+    public void testStationsPngAssetExists(){
+        assertTrue(Gdx.files.internal("Maps/Stations.png").exists(),"This will only pass if Stations.png exists");
+    }
+
+    @Test
+    public void testStationsTsxAssetExists(){
+        assertTrue(Gdx.files.internal("Maps/Stations.tsx").exists(),"This will only pass if Stations.tsx exists");
+    }
+
+    @Test
+    public void testStationsXcfAssetExists(){
+        assertTrue(Gdx.files.internal("Maps/Stations.xcf").exists(),"This will only pass if Stations.xcf exists");
+    }
+
+    @Test
+    public void testStationsMapPngAssetExists(){
+        assertTrue(Gdx.files.internal("Maps/StationsMap.png").exists(),"This will only pass if StationsMap.png exists");
+    }
+
+    @Test
+    public void testStationsMapTmxAssetExists(){
+        assertTrue(Gdx.files.internal("Maps/StationsMap.tmx").exists(),"This will only pass if StationsMap.tmx exists");
+    }
+
+    @Test
+    public void testStationsMap1AssetExists(){
+        assertTrue(Gdx.files.internal("Maps/StationsMap1.tmx").exists(),"This will only pass if StationsMap1.tmx exists");
+    }
+
+    @Test
+    public void testStationsMap2AssetExists(){
+        assertTrue(Gdx.files.internal("Maps/StationsMap2.tmx").exists(),"This will only pass if StationsMap2.tmx exists");
     }
 }
