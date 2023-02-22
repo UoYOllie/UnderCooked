1878700873fa637da711c3125efd6e9122f0f1ad
ajbrown <ab3073@york.ac.uk>
2023-02-22 09:38:40 +0000

    Adding tests in the customer and atlas asset folder

--- ENG1/tests/src/testing/AssetTests.java
+++ ENG1/tests/src/testing/AssetTests.java
@@ -8,6 +8,8 @@ import static org.testng.Assert.*;
 
 @RunWith(GdxTestRunner.class)
 public class AssetTests {
+
+    // The following are tests to do with checking that the assets in the cooks folder still remain
     @Test
     public void testChefHoldDownAssetExists(){
         assertTrue(Gdx.files.internal("cooks/hold down.png").exists(), "This test will only pass if hold down.png is present");
@@ -51,8 +53,27 @@ public class AssetTests {
     public void testChefNormalUpAssetExists(){
         assertTrue(Gdx.files.internal("cooks/normal up.png").exists(), "This test will only pass if normal up.png is present");
     }
+
+    //The following are tests to do with checking that assets in the customers folder remain
     @Test
-    public void testing(){
-        assertTrue(true, "1=1");
+    public void testCustomerAssetExists(){
+        assertTrue(Gdx.files.internal("customers/Customer.png").exists(),"This test will only pass if Customer.png exists in the customers folder");
+    }
+
+    //The following test that all assets in the atlas folder exist
+
+    @Test
+    public void testAtlasCustomerAssetExists(){
+        assertTrue(Gdx.files.internal("atlas/customer.png").exists(),"This test will only pass if customer.png exists in the atlas folder");
+    }
+
+    @Test
+    public void testAtlasCookAssetExists(){
+        assertTrue(Gdx.files.internal("atlas/cook.png").exists(),"This test will only pass if cook.png exists in the atlas folder");
+    }
+
+    @Test
+    public void testAtlasFoodAssetExists(){
+        assertTrue(Gdx.files.internal("atlas/food.png").exists(),"This test will only pass if food.png exists in the atlas folder");
     }
 }
