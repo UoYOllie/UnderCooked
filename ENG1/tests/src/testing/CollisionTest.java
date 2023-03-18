9f41eee54a42d12100c2a72725cba61770e82c5b
ajbrown <ab3073@york.ac.uk>
2023-03-18 18:05:53 +0000

    Slide against wall tests added :)

--- ENG1/tests/src/testing/CollisionTest.java
+++ ENG1/tests/src/testing/CollisionTest.java
@@ -66,21 +66,123 @@ public class CollisionTest {
     }
 
 
-    /**
+
     @Test
-    public void TestChefMoveUpLeftCollision(){
+    public void TestChefMoveSlideLeftUp(){
         Cook cook = new Cook(1500, 1200, 20, 20);
         ArrayList<Rectangle> testList = new ArrayList<>();
-        Rectangle rectangle = new Rectangle((1500 * 1/8f) - 20,(1200 * 1/8f) + 20,20,20);
+        Rectangle rectangle = new Rectangle((1500 * 1/8f) - 20,(1200 * 1/8f),20,20);
+        testList.add(rectangle);
+        keysPressed.add(InputKey.InputTypes.COOK_LEFT);
+        keysPressed.add(InputKey.InputTypes.COOK_UP);
+        cook.userInput(testList);
+        float final_move = (1500 * 1/8f);
+        float final_move2 = (1200 * 1/8f) + 1;
+        assertTrue(cook.getX()==final_move);
+        assertTrue(cook.getY()==final_move2);
+    }
+
+    @Test
+    public void TestChefMoveSlideLeftDown(){
+        Cook cook = new Cook(1500, 1200, 20, 20);
+        ArrayList<Rectangle> testList = new ArrayList<>();
+        Rectangle rectangle = new Rectangle((1500 * 1/8f) - 20,(1200 * 1/8f),20,20);
+        testList.add(rectangle);
+        keysPressed.add(InputKey.InputTypes.COOK_LEFT);
+        keysPressed.add(InputKey.InputTypes.COOK_DOWN);
+        cook.userInput(testList);
+        float final_move = (1500 * 1/8f);
+        float final_move2 = (1200 * 1/8f) - 1;
+        assertTrue(cook.getX()==final_move);
+        assertTrue(cook.getY()==final_move2);
+    }
+
+    @Test
+    public void TestChefMoveSlideRightUp(){
+        Cook cook = new Cook(1500, 1200, 20, 20);
+        ArrayList<Rectangle> testList = new ArrayList<>();
+        Rectangle rectangle = new Rectangle((1500 * 1/8f) + 21,(1200 * 1/8f),20,20);
+        testList.add(rectangle);
+        keysPressed.add(InputKey.InputTypes.COOK_RIGHT);
+        keysPressed.add(InputKey.InputTypes.COOK_UP);
+        cook.userInput(testList);
+        float final_move = (1500 * 1/8f) + 1;
+        float final_move2 = (1200 * 1/8f) + 1;
+        assertTrue(cook.getX()==final_move);
+        assertTrue(cook.getY()==final_move2);
+    }
+
+    @Test
+    public void TestChefMoveSlideRightDown(){
+        Cook cook = new Cook(1500, 1200, 20, 20);
+        ArrayList<Rectangle> testList = new ArrayList<>();
+        Rectangle rectangle = new Rectangle((1500 * 1/8f) + 21,(1200 * 1/8f),20,20);
+        testList.add(rectangle);
+        keysPressed.add(InputKey.InputTypes.COOK_RIGHT);
+        keysPressed.add(InputKey.InputTypes.COOK_DOWN);
+        cook.userInput(testList);
+        float final_move = (1500 * 1/8f) + 1;
+        float final_move2 = (1200 * 1/8f) - 1;
+        assertTrue(cook.getX()==final_move);
+        assertTrue(cook.getY()==final_move2);
+    }
+
+    @Test
+    public void TestChefMoveSlideUpRight(){
+        Cook cook = new Cook(1500, 1200, 20, 20);
+        ArrayList<Rectangle> testList = new ArrayList<>();
+        Rectangle rectangle = new Rectangle((1500 * 1/8f),(1200 * 1/8f) + 21,20,20);
+        testList.add(rectangle);
+        keysPressed.add(InputKey.InputTypes.COOK_UP);
+        keysPressed.add(InputKey.InputTypes.COOK_RIGHT);
+        cook.userInput(testList);
+        float final_move = (1500 * 1/8f) + 1;
+        float final_move2 = (1200 * 1/8f) + 1;
+        assertTrue(cook.getX()==final_move);
+        assertTrue(cook.getY()==final_move2);
+    }
+
+    @Test
+    public void TestChefMoveSlideUpLeft(){
+        Cook cook = new Cook(1500, 1200, 20, 20);
+        ArrayList<Rectangle> testList = new ArrayList<>();
+        Rectangle rectangle = new Rectangle((1500 * 1/8f),(1200 * 1/8f) + 21,20,20);
         testList.add(rectangle);
         keysPressed.add(InputKey.InputTypes.COOK_UP);
         keysPressed.add(InputKey.InputTypes.COOK_LEFT);
         cook.userInput(testList);
-        float final_move = (1500 * 1/8f) + 20;
-        float final_move2 = (1200 * 1/8f);
+        float final_move = (1500 * 1/8f) - 1;
+        float final_move2 = (1200 * 1/8f) + 1;
         assertTrue(cook.getX()==final_move);
         assertTrue(cook.getY()==final_move2);
     }
-    **/
 
+    @Test
+    public void TestChefMoveSlideDownRight(){
+        Cook cook = new Cook(1500, 1200, 20, 20);
+        ArrayList<Rectangle> testList = new ArrayList<>();
+        Rectangle rectangle = new Rectangle((1500 * 1/8f),(1200 * 1/8f) - 21,20,20);
+        testList.add(rectangle);
+        keysPressed.add(InputKey.InputTypes.COOK_DOWN);
+        keysPressed.add(InputKey.InputTypes.COOK_RIGHT);
+        cook.userInput(testList);
+        float final_move = (1500 * 1/8f) + 1;
+        float final_move2 = (1200 * 1/8f) - 1;
+        assertTrue(cook.getX()==final_move);
+        assertTrue(cook.getY()==final_move2);
+    }
+
+    public void TestChefMoveSlideDownLeft(){
+        Cook cook = new Cook(1500, 1200, 20, 20);
+        ArrayList<Rectangle> testList = new ArrayList<>();
+        Rectangle rectangle = new Rectangle((1500 * 1/8f),(1200 * 1/8f) - 21,20,20);
+        testList.add(rectangle);
+        keysPressed.add(InputKey.InputTypes.COOK_DOWN);
+        keysPressed.add(InputKey.InputTypes.COOK_RIGHT);
+        cook.userInput(testList);
+        float final_move = (1500 * 1/8f) - 1;
+        float final_move2 = (1200 * 1/8f) - 1;
+        assertTrue(cook.getX()==final_move);
+        assertTrue(cook.getY()==final_move2);
+    }
 }
