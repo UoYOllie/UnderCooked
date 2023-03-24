75a1268aefd5d83aa8e0e8db453b18a46b2d03f7
Oliver <oj613@york.ac.uk>
2023-03-24 15:24:08 +0000

    Fixed some things
    Added:
    + Added help.html with some basic notes I made whilst fixing.
    + Added a couple interactables to the map for testing.
    + Added new station to the map helped (Pantry)
    + Added an ItemRegister kind of thing to FoodItem
    + Added separate method for interacting with objects to avoid conflict with Unit Tests.
    
    Changed:
    * Changed how rendering player item stacks works.
    * Changed how interacting with stations works.
    
    Fixed:
    / Fixed bug where interacting with stuff would cause a CTD.
    / Fixed issue where stationID caused null pointer exception.
    
    Removed:
    - Removed GameScreen instance dependency for Cook.
    - Removed duplicate constructor for cook.
    
    THE FOLLOWING NEEDS DOING:
    
    • StationID is stupid and I hate it. - Remove it or make a station register for them.
    • Rendering is enabled but not perfected at all - values need tweaking etc.
    • Anywhere "1/8f" is mentioned should almost always be replaced with "Constants.UnitScale" in case we need to change this value at any point in the future.

---
+++ ENG1/tests/build/resources/main/OJAssets/MFPack/Undercooked.tsx
@@ -0,0 +1,92 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="Undercooked" tilewidth="1000" tileheight="2048" tilecount="30" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+
+ <tile id="1">
+  <image width="1000" height="1000" source="SpaceMEGAtile.png"/>
+ </tile>
+ <tile id="2">
+  <image width="32" height="32" source="Stars.png"/>
+ </tile>
+ <tile id="3">
+  <image width="32" height="32" source="Window-Starship-withbluggy.png"/>
+ </tile>
+ <tile id="4">
+  <image width="32" height="32" source="Window-Starship.png"/>
+ </tile>
+ <tile id="5">
+  <image width="384" height="192" source="Dome.png"/>
+ </tile>
+ <tile id="6">
+  <image width="32" height="32" source="DomeCounter.png"/>
+ </tile>
+ <tile id="7">
+  <image width="32" height="32" source="DomeCounterInv.png"/>
+ </tile>
+ <tile id="8">
+  <image width="64" height="32" source="ServingStation.png"/>
+ </tile>
+ <tile id="9">
+  <image width="78" height="62" source="Dinosaur.png"/>
+ </tile>
+ <tile id="10">
+  <image width="32" height="32" source="TileFloorMF.png"/>
+ </tile>
+ <tile id="11">
+  <image width="32" height="32" source="Purpadurple.png"/>
+ </tile>
+ <tile id="12">
+  <image width="78" height="62" source="Shop.png"/>
+ </tile>
+ <tile id="13">
+  <image width="32" height="32" source="Rarch.png"/>
+ </tile>
+ <tile id="14">
+  <image width="32" height="32" source="Larch.png"/>
+ </tile>
+ <tile id="15">
+  <image width="32" height="32" source="CurvyMetal.png"/>
+ </tile>
+ <tile id="16">
+  <image width="32" height="32" source="MetalBlock.png"/>
+ </tile>
+ <tile id="17">
+  <image width="64" height="32" source="MetalBlock2.png"/>
+ </tile>
+ <tile id="18">
+  <image width="64" height="32" source="MetalBlock2flipped.png"/>
+ </tile>
+ <tile id="19">
+  <image width="64" height="32" source="MetalSlope2flipped.png"/>
+ </tile>
+ <tile id="20">
+  <image width="64" height="32" source="MetalSlope2.png"/>
+ </tile>
+ <tile id="21">
+  <image width="32" height="32" source="MetalBlockflipped.png"/>
+ </tile>
+ <tile id="22">
+  <image width="32" height="32" source="CurvyMetalflipped.png"/>
+ </tile>
+ <tile id="23">
+  <image width="64" height="96" source="EmptyVAT.png"/>
+ </tile>
+ <tile id="24">
+  <image width="64" height="96" source="TopVAT.png"/>
+ </tile>
+ <tile id="25">
+  <image width="96" height="32" source="SignPantry.png"/>
+ </tile>
+ <tile id="26">
+  <image width="96" height="32" source="SignFridge.png"/>
+ </tile>
+ <tile id="27">
+  <image width="96" height="32" source="SignDining.png"/>
+ </tile>
+ <tile id="28">
+  <image width="64" height="96" source="BottomVAT.png"/>
+ </tile>
+ <tile id="29">
+  <image width="256" height="2048" source="Beam.png"/>
+ </tile>
+</tileset>
