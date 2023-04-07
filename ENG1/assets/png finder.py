88075b6c97a5161921f51c3d637ffc7ad78b0716
Oliver <oj613@york.ac.uk>
2023-04-07 15:49:52 +0100

    Added comments to python script

--- ENG1/assets/png finder.py
+++ ENG1/assets/png finder.py
@@ -9,29 +9,28 @@ def find_png(directory):
         directory = "."
 
 
-    for file in current_directory:
-        if (".png" in file) or (".atlas" in file):
+    for file in current_directory: # Searches for assets in current directory
+        if (".png" in file) or (".atlas" in file): # Which file extensions to look for.
             list_of_pngs.append((directory + "/" + file)[2:])
 
-    print(current_directory)
 
-    for file in current_directory:
-        if ("." not in file):
+    for file in current_directory: # Searches through all the folders.
+        if ("." not in file): # This script assumes any file without an extension (or ".") is a folder
             print("going to:", directory + "/" + file)
-            new_list = find_png(directory + "/" + file)
+            new_list = find_png(directory + "/" + file) # Goes one folder deeper and starts again.
 
             for a in new_list:
-                list_of_pngs.append(a)
+                list_of_pngs.append(a) # appends all items from the deeper recursion to the current list of assets.
             
 
-    return list_of_pngs
+    return list_of_pngs # Returns list of assets in the current directory (inc. all in nested folders)
 
 
 output_list = find_png("./")
 
 output = open("assets.txt", "w")
 
-for asset in output_list:
+for asset in output_list: # Writes list of assets to 'assets.txt'
     output.write(asset + "\n")
 
 output.close()
