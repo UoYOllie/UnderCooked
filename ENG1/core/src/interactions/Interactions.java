dda10a3de0f8b32e7f54cabbe5323500262390cf
laura <lmlb500@york.ac.uk>
2023-04-09 13:03:26 +0100

    - flipping at the frying station

--- ENG1/core/src/interactions/Interactions.java
+++ ENG1/core/src/interactions/Interactions.java
@@ -25,10 +25,10 @@ public class Interactions {
         interactions.put(InteractionKey(FoodID.lettuce, StationID.cut), new InteractionResult(FoodID.lettuceChop,new float[] {},13F));
         interactions.put(InteractionKey(FoodID.tomato, StationID.cut), new InteractionResult(FoodID.tomatoChop,new float[] {},13F));
         interactions.put(InteractionKey(FoodID.onion, StationID.cut), new InteractionResult(FoodID.onionChop,new float[] {},13F));
-        interactions.put(InteractionKey(FoodID.meat, StationID.fry), new InteractionResult(FoodID.meatCook,new float[] {50},13F));
+        interactions.put(InteractionKey(FoodID.meat, StationID.fry), new InteractionResult(FoodID.meatCook,new float[] {},13F));
         interactions.put(InteractionKey(FoodID.potato, StationID.bake), new InteractionResult(FoodID.potatoCook,new float[] {},13F));
         interactions.put(InteractionKey(FoodID.dough, StationID.bake), new InteractionResult(FoodID.doughCook,new float[] {},13F));
-        interactions.put(InteractionKey(FoodID.tomatoChop, StationID.fry), new InteractionResult(FoodID.tomatoSauce,new float[] {50},13F));
+        interactions.put(InteractionKey(FoodID.tomatoChop, StationID.fry), new InteractionResult(FoodID.tomatoSauce,new float[] {},13F));
     }
 
     /**
