f2ce7f768cf4be0d178d9aff7e62754614573163
Morgan <mf1231@york.ac.uk>
2023-05-01 18:38:58 +0100

    RepPoints.java Documented

--- ENG1/core/src/customers/RepPoints.java
+++ ENG1/core/src/customers/RepPoints.java
@@ -1,12 +1,23 @@
+//DC
 package customers;
 
 public class RepPoints {
     private int Points;
 
+    /**
+     * Constructor
+     * Sets inital points to 3 as stated in requirements
+     */
     public RepPoints(){
         this.Points = 3;
     }
 
+    /**
+     * Sets point
+     * sets this.points to x
+     * Also checks if points>5 then sets point only up to a maximum of 5
+     * @param x
+     */
     public void setPoints(int x)
     {
         if(x>=5)
@@ -19,12 +30,21 @@ public class RepPoints {
         }
     }
 
-
+    /**
+     * Reduces this.point by 1 when ran.
+     * Used when customer runs StormOut()
+     * Only used when customers leave without food
+     */
     public void Negative()
     {
         this.Points = this.Points - 1;
     }
 
+    /**
+     * Increases this.points by 1
+     * Used when cutomer gets their food
+     * Used when customer runs Success function before leaving
+     */
     public void Positive()
     {
         this.Points++;
@@ -34,6 +54,10 @@ public class RepPoints {
         }
     }
 
+    /**
+     * getter for this.points
+     * @return current reputation points
+     */
     public int getPoints(){
         return this.Points;
     }
