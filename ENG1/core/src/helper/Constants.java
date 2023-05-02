53372e83b1d38dbc853fdb0d615878d40ae93488
Morgan <mf1231@york.ac.uk>
2023-05-02 15:42:14 +0100

    WE FIXED THE DOOR!!

--- ENG1/core/src/helper/Constants.java
+++ ENG1/core/src/helper/Constants.java
@@ -31,7 +31,7 @@ public class Constants {
     public static final float customerSplitPoint = 2817.9171f;
 
     // customer travelling down the corridor
-    public static final Vector2 customerPointA = new Vector2 (1978.075f, 2915.9f);
+    public static final Vector2 customerPointA = new Vector2 (1978.075f+10f, 2915.9f+12f);
     public static final Vector2 customerPointB = new Vector2 (1978.075f, 2913f);
     public static final Vector2 customerPointC = new Vector2 (1939f, 2913f);
     public static final Vector2 customerPointD = new Vector2 (1939.4f, 2872f);
